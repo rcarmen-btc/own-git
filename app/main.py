53285eaeb649aba91e9d24ce9d43510bcbadba65
riser <riserlarenss@gmail.com>
2022-05-13 12:07:02 +0300

    done third stage

--- app/main.py
+++ app/main.py
@@ -18,6 +18,7 @@ def main():
         os.mkdir(".git")
         os.mkdir(".git/objects")
         os.mkdir(".git/refs")
+        
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
