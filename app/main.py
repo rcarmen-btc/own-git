97bc13ad636f1022c57f4e387f965f83e34c0ee2
riser <riserlarenss@gmail.com>
2022-05-13 11:56:52 +0300

    done third stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
+from importlib.resources import path
 import sys
 import os
+from tkinter import W
 import zlib
+import hashlib
+
+from numpy import size
 
 
 def main():
@@ -16,14 +21,37 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
-    elif command == "cat-file" and sys.argv[2] == "-p":
-        sha1 = sys.argv[3]
-        dir_name = sha1[:2]
-        file_name = sha1[2:]
-        with open(f'.git/objects/{dir_name}/{file_name}', 'rb') as f:
-            data = f.read()
-            decomp_data = zlib.decompress(data).split(b'\x00')[1].decode("utf-8")
-            print(decomp_data, end="")
+    elif command == "cat-file":
+        if  sys.argv[2] == "-p":
+            sha1 = sys.argv[3]
+            dir_name = sha1[:2]
+            file_name = sha1[2:]
+            with open(f'.git/objects/{dir_name}/{file_name}', 'rb') as f:
+                data = f.read()
+                decomp_data = zlib.decompress(data).split(b'\x00')[1].decode("utf-8")
+                print(decomp_data, end="")
+    elif command == "hash-object":
+        if sys.argv[2] == "-w":
+            file_name = sys.argv[3]
+            with open(file_name, 'r') as f:
+                data = f.read()
+            header = f"blob {len(data.encode('utf-8'))}\0"
+            store = header + data
+
+            sha1 = hashlib.sha1(store.encode()).hexdigest()
+            print(sha1)
+
+            dir_name = sha1[:2]
+            file_name = sha1[2:]
+            path_to_dir = f'.git/objects/{dir_name}/'
+
+            try:
+                os.mkdir(path_to_dir)
+                zlib_content = zlib.compress(store.encode())
+                with open(path_to_dir + file_name, 'wb') as f:
+                    f.write(zlib_content)
+            except FileExistsError:
+                pass
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
